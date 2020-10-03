88343168ea5cb06777cb05c80a962e4e218c6775
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-03 18:12:16 -0400

    Lay out header

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import './bootstrap.min.css';
+import './bootstrap.min.css'
+import './layout.css'
+
+import Navbar from './Globals/Navbar'
 
 const Layout = ({ children }) => (
     <>
+        <Navbar/>
         {children}
     </>
 )
