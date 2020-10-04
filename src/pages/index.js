6bcd8bd78f9c5258786f7229a2e41a5724ad8f71
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-10-03 22:58:01 -0400

    Introduce background section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-import {FaGulp} from 'react-icons/fa'
+import BackgroundSection from '../components/Globals/BackgroundSection'
 
-const IndexPage = () => (
+
+const IndexPage = ({data}) => (
     <Layout>
-        <SEO title="Home" />
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <FaGulp/>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+        <BackgroundSection img={data.img.childImageSharp.fluid} title="regular Joe's" styleClass='default-background' />
+        <h3>Blabla</h3>
     </Layout>
 )
+
+export const query = graphql `{
+    img: file(relativePath: {eq:"default-background.jpg"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }`
 
 export default IndexPage
